2cdc11d9b3ead10c2757ee7d58eaee6a32e1e461
Luis Olivo <luisaolivo@yahoo.com>
2012-04-19 20:50:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orbital::Application.config.secret_token = '5549a9c6ed96bd618187d620e8fb2f5bbb1078c7cdc810146236e87b9f5f4044b696e708bda7991206a80a753f680958598036670533b13500f29461f7e39073'
